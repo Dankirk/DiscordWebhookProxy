f30442d1323d23420109e7f0191d78bd668f7b51
Henri Aho <dankirk_88@msn.com>
2018-04-11 01:18:51 +0300

    Comments for example.php

--- example.php
+++ example.php
@@ -1,9 +1,13 @@
 <?php
 	require_once(__DIR__ . "/DiscordWebhook.php");
 
-	$webhook_id = "id_you_from_discord";
+	// Discord webhook id and token should be stored somewhere safe.
+	// These are here for demonstration purposes.
+	$webhook_id = "id_you_got_from_discord";
 	$webhook_token = "token_you_got_from_discord";
 
+	// Some form of security is recommended to prevent webhook abuse.
+	// Discord relies on the token being secret, so this should be enough for us.
 	if (!isset($_GET['secret']) || $_GET['secret'] !== $webhook_token)
 		exit();
 
@@ -13,6 +17,8 @@
 		$webhook->proxy('Bitbucket');
 	}
 	catch (DiscordWebhookException $e) {
+
+		// Log errors. You may want to do something else here.
 		error_log($e->getMessage());
 	}
 ?>
